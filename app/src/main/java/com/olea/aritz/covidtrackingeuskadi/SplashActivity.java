de174774cc6c8826c2d578d1347b690ba298809e
Aritz Olea <aritzolea97@gmail.com>
2021-01-16 23:03:44 +0100

    [ADD] Splash screen

---
+++ app/src/main/java/com/olea/aritz/covidtrackingeuskadi/SplashActivity.java
@@ -0,0 +1,67 @@
+package com.olea.aritz.covidtrackingeuskadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Animation topAnimation, bottomAnimation;
+
+    ImageView splashImageView;
+    TextView splashTitle, splashSubtitle, loadingText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        topAnimation.setAnimationListener(new Animation.AnimationListener(){
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                loadingText.setAlpha(1.0f);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        splashImageView = findViewById(R.id.splashImageView);
+        splashTitle = findViewById(R.id.splashTitle);
+        splashSubtitle = findViewById(R.id.splashSubtitle);
+        loadingText = findViewById(R.id.loadingText);
+
+        splashImageView.setAnimation(bottomAnimation);
+        splashTitle.setAnimation(topAnimation);
+        splashSubtitle.setAnimation(topAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 5000); //TODO: Lo hace por tiempo, habrá que hacerlo cuando los datos estén cargados
+    }
+}
